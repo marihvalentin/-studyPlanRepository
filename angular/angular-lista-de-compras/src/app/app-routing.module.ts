9a9e30e28df8821dd15d5ebe5d207b206e3492d5
Mariana Valentim Martins <mariana.martins@GEO.INTRANET>
2020-11-06 18:41:46 -0300

    adicionada dependência de itenscomponent e rota no array routes para itenscomponent

--- angular/angular-lista-de-compras/src/app/app-routing.module.ts
+++ angular/angular-lista-de-compras/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItensComponent } from './itens/itens.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'itens', component: ItensComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
