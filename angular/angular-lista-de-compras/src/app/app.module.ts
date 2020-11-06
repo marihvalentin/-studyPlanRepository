6aba43e6a369ad1524e8b292a9b6a926322b2be5
Mariana Valentim Martins <mariana.martins@GEO.INTRANET>
2020-11-06 17:10:12 -0300

    Adicionado componente itens e incluso estilo da folha de estilo default

--- angular/angular-lista-de-compras/src/app/app.module.ts
+++ angular/angular-lista-de-compras/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItensComponent } from './itens/itens.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItensComponent
   ],
   imports: [
     BrowserModule,
