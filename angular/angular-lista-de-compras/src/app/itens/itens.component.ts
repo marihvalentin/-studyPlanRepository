56e833d9960da4227b62652d7f1b71857291bbbf
Mariana Valentim Martins <mariana.martins@GEO.INTRANET>
2020-11-08 19:04:38 -0300

    atualizações realizada

--- angular/angular-lista-de-compras/src/app/itens/itens.component.ts
+++ angular/angular-lista-de-compras/src/app/itens/itens.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../item';
 import { ItemService } from '../item.service';
-import { MensagemService} from '../mensagem.service';
+import { MensagemService } from '../mensagem.service';
 
 @Component({
   selector: 'app-itens',
@@ -11,15 +11,14 @@ import { MensagemService} from '../mensagem.service';
 export class ItensComponent implements OnInit {
 
   itens: Item[];
-  //itens = ITENS;
+
+  constructor(private itemService: ItemService, private mensagemService: MensagemService) { }
 
   selectedItem: Item;
   onSelect(item: Item): void {
     this.selectedItem = item;
     this.mensagemService.add(`ItensComponent: Selecionado item de ID=${item.id}`);
   }
-
-  constructor(private itemService: ItemService, private mensagemService: MensagemService) { }
 
   getItens(): void {
     this.itemService.getItens().subscribe(itens => this.itens = itens);
