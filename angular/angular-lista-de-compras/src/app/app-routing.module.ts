39d8de7847bf2d271fc1627fa941303e24a02138
Mariana Valentim Martins <mariana.martins@GEO.INTRANET>
2020-11-08 18:49:53 -0300

    alteração do modulo de rotas com novas rotas para navegação

--- angular/angular-lista-de-compras/src/app/app-routing.module.ts
+++ angular/angular-lista-de-compras/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetalheItemComponent } from './detalhe-item/detalhe-item.component';
 import { ItensComponent } from './itens/itens.component';
 
 const routes: Routes = [
-  {path: 'itens', component: ItensComponent}
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}, //rota padrão
+  {path: 'itens', component: ItensComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'detalhe/:id', component: DetalheItemComponent}
 ];
 
 @NgModule({
