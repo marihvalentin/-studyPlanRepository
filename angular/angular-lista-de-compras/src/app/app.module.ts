5aad713cc3a4e5d6d3a84f831812b94e0776b811
Mariana Valentim Martins <mariana.martins@GEO.INTRANET>
2020-11-08 16:04:04 -0300

    adicionadas importações referentes ao procedimento de habilitar os serviços http

--- angular/angular-lista-de-compras/src/app/app.module.ts
+++ angular/angular-lista-de-compras/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItensComponent } from './itens/itens.component';
+import { DetalheItemComponent } from './detalhe-item/detalhe-item.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItensComponent
+    ItensComponent,
+    DetalheItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
