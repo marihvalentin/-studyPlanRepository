236d60b1f2d3d6d8d5d2717084955fa56ed23512
Mariana Valentim Martins <mariana.martins@GEO.INTRANET>
2020-11-08 17:57:44 -0300

    atualização com imports e declarations referentes ao mensagemService e ao FormsModule

--- angular/angular-lista-de-compras/src/app/app.module.ts
+++ angular/angular-lista-de-compras/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItensComponent } from './itens/itens.component';
@@ -9,16 +9,19 @@ import { DetalheItemComponent } from './detalhe-item/detalhe-item.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
+import { MensagensComponent } from './mensagens/mensagens.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItensComponent,
-    DetalheItemComponent
+    DetalheItemComponent,
+    MensagensComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
