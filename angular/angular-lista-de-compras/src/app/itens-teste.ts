99ab6d9f481cc993350f3c611f648688cb478c02
Mariana Valentim Martins <mariana.martins@GEO.INTRANET>
2020-11-08 16:37:32 -0300

    criado arquivo com itens teste da lista de compras, com um array de 5 itens

---
+++ angular/angular-lista-de-compras/src/app/itens-teste.ts
@@ -0,0 +1,9 @@
+import { Item } from './item';
+
+export const ITENS: Item[] = [
+    {id: 1, qtd: '5kg', descricao: 'Arroz'},
+      {id: 2, qtd: '500g', descricao: 'Farofa'},
+      {id: 3, qtd: '4L', descricao: 'Leite'},
+      {id: 4, qtd: '700g', descricao: 'Bacon'},
+      {id: 5, qtd: '3kg', descricao: 'Açúcar'}
+];
